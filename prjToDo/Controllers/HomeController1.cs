83ab76d81f13dd05d5169aba308f5f938c202f9e
SunnyCheng <pei.shine111@gmail.com>
2023-05-04 17:26:46 +0800

    0504 page2-16

---
+++ prjToDo/Controllers/HomeController1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using prjToDo.Models;
+
+namespace prjToDo.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        dbToDoEntities1 db = new dbToDoEntities1 ();
+        // GET: HomeController1
+        public ActionResult Index()
+        {
+            var todos = db.tToDo.OrderByDescending(m => m.fDate).ToList();
+
+            return View(todos);
+        }
+    }
+}
