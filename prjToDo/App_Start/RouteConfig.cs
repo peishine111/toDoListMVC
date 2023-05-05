f17f3034fe18c1e5bc1a61c87e7161a61f779b5c
SunnyCheng <pei.shine111@gmail.com>
2023-05-05 10:22:59 +0800

    error 404

--- prjToDo/App_Start/RouteConfig.cs
+++ prjToDo/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace prjToDo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "HomeController1", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
